b7528bee4841fb16c8fed2af5a69207d2ed75d49
stephen chelimo <stephenchelimo057@gmail'com>
2020-02-29 20:22:12 +0300

    javascript

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    // $(".content1").hover(function() {
+    //     $(".contant1").toggle("slow");
+    //     $(".content2").toggle("5000ms");
+    // });
+
+
+
+    const pizzaSizeArray = [{
+            name: "Extra Large",
+            price: 900
+        },
+        {
+            name: "Large",
+            price: 700
+        },
+        {
+            name: "Medium",
+            price: 500
+        },
+        {
+            name: "Small",
+            price: 300
+        }
+    ]
+
+    const crustOptionArray = [{
+            name: "Double-Delight",
+            price: 300
+        },
+        {
+            name: "Regular",
+            price: 0
+        }
+    ]
+
+    const toppingsOptionArray = [{
+            name: "BBQ Chicken",
+            price: 300
+        },
+        {
+            name: "Meat Deluxe",
+            price: 200
+        },
+        {
+            name: "Margherita",
+            price: 250
+        }
+    ]
+
