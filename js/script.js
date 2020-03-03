b7c6906a9fc843364587d5a1b1381a5d724bcdf7
stephen chelimo <stephenchelimo057@gmail'com>
2020-03-03 15:17:34 +0300

    menu preparation

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ $(document).ready(function () {
         var number = $("input#number").val();
         var location = $("input#location").val();
 
-        alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + " within one hour.The delivery will cost ksh 180/= Thank you for chosing the pizzeria.");
+        alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + " within one hour.The delivery will cost ksh 50.<br> thanks from pizza-pap.");
         // $(this).get(0).reset();
         //  event.preventDefault();
     });
