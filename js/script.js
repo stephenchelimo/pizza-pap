d12ae37016b2ca60ef0f793a9b81443b1160e46f
stephen chelimo <stephenchelimo057@gmail'com>
2020-03-03 15:07:50 +0300

    menu preparation

--- js/script.js
+++ js/script.js
@@ -1,156 +1,81 @@
-$(document).ready(function() {
-    const pizzaSizeArray = [{
-            name: "Extra Large",
-            price: 900
-        },
-        {
-            name: "Large",
-            price: 700
-        },
-        {
-            name: "Medium",
-            price: 500
-        },
-        {
-            name: "Small",
-            price: 300
-        }
-    ]
+function getSizeCost() {
+    var selectedSize = document.getElementById("size").value;
+    return parseInt(selectedSize);
+}
 
-    const crustOptionArray = [{
-            name: "Double-Delight",
-            price: 300
-        },
-        {
-            name: "Regular",
-            price: 0
-        }
-    ]
+function getCrustCost() {
+    var selectedCrust = document.getElementById("crust").value;
+    return parseInt(selectedCrust);
+}
+function getNumber() {
+    var selectedNumber = document.getElementById("numberofpizza").value;
+    return parseInt(selectedNumber);
+}
+function mushrooms() {
+    var mushroom = 0;
+    var addMushroom = document.getElementById("toppingone");
+    if (addMushroom.checked === true) {
+        mushroom = 150;
+    }
+    return parseInt(mushroom);
+}
 
-    const toppingsOptionArray = [{
-            name: "BBQ Chicken",
-            price: 300
-        },
-        {
-            name: "Meat Deluxe",
-            price: 200
-        },
-        {
-            name: "Margherita",
-            price: 250
-        }
-    ]
-    $("#orderPizza").submit(function(event) {
-        event.preventDefault();
+function sausages() {
+    var sausage = 0;
+    var addsausage = document.getElementById("toppingone");
+    if (addsausage.checked === true) {
+        sausage = 140;
+    }
+    return parseInt(sausage);
+}
+function onions() {
+    var onion = 0;
+    var addonions = document.getElementById("toppingone");
+    if (addMushroom.checked === true) {
+        onion = 130;
+    }
+    return parseInt(onion);
+}
+function tikkaSaucee() {
+    var tikkaSauce = 0;
+    var addtikkaSauce = document.getElementById("toppingone");
+    if (addtikkaSauce.checked === true) {
+        tikkaSauce = 120;
+    }
+    return parseInt(tikkaSauce);
+}
+function mozarellaCheese() {
+    var cheese = 0;
+    var addmozarellaCheese = document.getElementById("toppingone");
+    if (addmozarellaCheese.checked === true) {
+        cheese = 110;
+    }
+    return parseInt(cheese);
+}
 
-        pizzaSize = $("#pizzaSize").val();
-        crustOption = $("#crustOption").val();
-        toppingsOption = $("#toppings").val();
-        extraToppings = $("#extraToppings").val();
-        numberOfOrder = $("#numberOfOrders").val();
+function calctotalPrice(e) {
+    event.preventDefault();
+    var totalPrice = (getSizeCost() + getCrustCost() + mushrooms()) * (getNumber());
+    
+        console.log(totalPrice);
+    alert("Your order of " + getNumber() + " pizzas has been processed.Your total amount payable is " + totalPrice +"." )
 
-        let pizzaSizePrice = 0;
-        let crustOptionPrice = 0;
-        let toppingsOptionPrice = 0;
-        let extraToppingsPrice = 0;
-        //to use these variables later
+}
 
-
-
-        //find out which size was selected
-
-        $.each(pizzaSizeArray, function(key, value) {
-            if (value.name == pizzaSize) {
-                console.log(value.name + value.price);
-                pizzaSizePrice = value.price;
-                return pizzaSizePrice
-            }
-        });
-
-
-        //find out which crust was selected
-        console.log(pizzaSizePrice)
-        $.each(crustOptionArray, function(key, value) {
-            if (value.name == crustOption) {
-                console.log(value.name + value.price);
-                crustOptionPrice = value.price;
-                return crustOptionPrice
-            }
-        });
-
-
-        //find out which topping was selected
-        $.each(toppingsOptionArray, function(key, value) {
-            if (value.name == toppingsOption) {
-                console.log(value.name + value.price);
-                toppingsOptionPrice = value.price;
-                return toppingsOptionPrice
-            }
-        });
-
-
-        console.log(extraToppings)
-        if (extraToppings == "Familiar") {
-            extraToppingsPrice = 0;
-            console.log(extraToppingsPrice);
-        } else {
-            if (extraToppings == "Surprise Me" && pizzaSize == "Extra Large") {
-                extraToppingsPrice = 250;
-                console.log(extraToppingsPrice);
-            } else if (extraToppings == "Surprise Me" && pizzaSize == "Large") {
-                extraToppingsPrice = 200;
-                console.log(extraToppingsPrice);
-            } else if (extraToppings == "Surprise Me" && pizzaSize == "Medium") {
-                extraToppingsPrice = 150;
-                console.log(extraToppingsPrice);
-            } else if (extraToppings == "Surprise Me" && pizzaSize == "Small") {
-                extraToppingsPrice = 100;
-                console.log("extraToppings ", extraToppingsPrice)
-            }
-        }
-
-
-        //total price calculation (size, crust, toppings, extraToppings)
-        const totalCostOfPizza = pizzaSizePrice + crustOptionPrice + toppingsOptionPrice + extraToppingsPrice;
-        console.log("total ", totalCostOfPizza);
-
-
-        //total with number of orders
-
-        const finalTotal = totalCostOfPizza * numberOfOrder;
+// Delivery form inputs and alerts
 
 
 
-        console.log(finalTotal);
+$(document).ready(function () {
+    $("#delivery").submit(function () {
 
-        $("#sizeAndPrice").html("Size and Price: " + pizzaSizePrice);
-        $("#crustAndPrice").html("Crust and Price: " + crustOptionPrice);
-        $("#toppingsAndPrice").html("Toppings and Price: " + toppingsOptionPrice);
-        $("#extraToppingsAndPrice").html("Extra Toppings and Price: " + extraToppingsPrice);
-        $("#subTotal").html("Subtotal Cost: " + totalCostOfPizza);
-        $("#orderNumber").html("Number of Order(s): " + numberOfOrder);
-        $("#grandTotal").html("Grand Total Cost: " + finalTotal);
+        var name = $("input#name").val();
+        var number = $("input#number").val();
+        var location = $("input#location").val();
 
+        alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + " within one hour.The delivery will cost ksh 180/= Thank you for chosing the pizzeria.");
+        // $(this).get(0).reset();
+        //  event.preventDefault();
     });
-    $("#deliveryPointForm").submit(function(event) {
-        event.preventDefault();
-
-        deliveryPoint = $("#deliveryPoint").val();
-        if (deliveryPoint == 1) {
-            $("#remoteLocation").show();
-
-        };
-
-    });
- $("#submitLocaton").submit(function(event) {
-        event.preventDefault();
-        var remoteClientName = $("#userName").val();
-        var remoteClientAddress = $("#userAddress").val();
-        var remoteClientPhoneNumber = $("#userTelNum").val();
-
-        alert("#remoteThankYou").html("Thankyou " + remoteClientName + " for odering with us! Your order will be delivered to your location, " + remoteClientAddress + ".");
-
-    });
-
-
+    
 });
